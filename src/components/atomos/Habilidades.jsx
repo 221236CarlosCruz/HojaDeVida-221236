a5d60c439fb832891d900b3756dd9b7a83d99f5a
221236CarlosCruz <221236@ids.upchiapas.edu.mx>
2023-01-21 11:37:49 -0600

    [ADD] component Habilidades

---
+++ src/components/atomos/Habilidades.jsx
@@ -0,0 +1,18 @@
+import IconoHabilidades from '../../assets/FotoHabilidades.png'
+import '../../assets/styles/Habilidades.css'
+
+function Habilidades() {
+    return (
+        <div>
+            <img src={IconoHabilidades} className="FotoHabilidades" />
+            <h1 className='TextHabilidades'>HABILIDADES</h1>
+            <h3 className='Descripcion2'>HTML <br />
+            CSS <br />
+            JAVA <br />
+            C++ <br />
+            JAVASCRIPT</h3>
+        </div>
+    );
+}
+
+export default Habilidades;
--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,11 +1,13 @@
 import Header from "../components/atomos/Header"
 import PerfilPersonal from "../components/atomos/PerfilPersonal";
+import Habilidades from "../components/atomos/Habilidades";
 
 function App() {
     return (
         <>
         <Header></Header>
         <PerfilPersonal></PerfilPersonal>
+        <Habilidades></Habilidades>
         </>
     );
 }
